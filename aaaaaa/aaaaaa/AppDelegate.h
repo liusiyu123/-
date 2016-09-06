8dab90f4e6d0e4cc39ad63e1302eba3fbbe08a37
feng <295441953@qq.com>
2016-09-06 10:06:29 +0800

    原型

---
+++ aaaaaa/aaaaaa/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  aaaaaa
+//
+//  Created by jutehui on 16/9/6.
+//  Copyright © 2016年 jutehui. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ aaaaaa/aaaaaa/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  aaaaaa
+//
+//  Created by jutehui on 16/9/6.
+//  Copyright © 2016年 jutehui. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
